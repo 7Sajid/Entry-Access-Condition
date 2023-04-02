8813abe663ea432aa54fe0ba3953b624b7eb53b8
Rafat Ul Aman Sajid <45040449+7Sajid@users.noreply.github.com>
2023-04-02 21:59:27 +0600

    Add files via upload

---
+++ Acces To Ticket (condition).py
@@ -0,0 +1,14 @@
+print("!!! Welcome to the rollercoster !!!!")
+height =int(input("Write Down Your Height In cm : "))
+if height > 120:
+  print("You Can Ride The Rollercoster ,You Are over 120cm " )
+else :
+  print("You Cannot Ride The Rollercoster,You Need Atlest 120Cm for Ride")
+
+print("!!! Welcome to the Boxing Club !!!!")
+height =int(input("Write down Your Weight in kg: "))
+if height >= 60:
+  print("You Can Join Our Club " )
+else :
+  print("You Are underweight . Gain Muscles and come back later")
+
